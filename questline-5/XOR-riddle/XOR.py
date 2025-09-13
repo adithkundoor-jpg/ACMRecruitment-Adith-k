3228fe161a308e7f07e9e6e7c018533a31c99d18
adithkundoor-jpg <adithkundoor@gmail.com>
2025-09-13 21:25:32 +0530

    Add files via upload

---
+++ questline-5/XOR-riddle/XOR.py
@@ -0,0 +1,8 @@
+N=58
+xor_value=23
+# perform XOR operation
+result = N ^ xor_value
+if result ==45:
+    print(f"verification succesful! {N} XOR {xor_value}={result}")
+else:
+    print(f"verification failed. {N} XOR {xor_value}={result},not 45")
